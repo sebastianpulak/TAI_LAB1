5c65ea3ec8b62090f975bdea3fa3d83f9ec4ec8f
Sebastian Pulak <pulakosso@gmail.com>
2019-03-08 18:17:06 +0100

    dsada

---
+++ multTable.js
@@ -0,0 +1,10 @@
+let x = "";
+
+for(let i=1;i<11;i++){
+    for(let j=1;j<11;j++){
+        x+=i*j + ' ';
+    }
+    x += '\n';
+}
+console.log(x);
+
---
+++ navbar.js
@@ -0,0 +1,9 @@
+let nav = 
+'<nav>'+
+'<ul class="nav">'+
+        '<li class="nav-item"><a class="nav-link" href="#">Home</a></li>'+
+        '<li class="nav-item"><a class="nav-link" href="#">Blog</a></li>'+
+        '<li class="nav-item"><a class="nav-link" href="#">Quiz</a></li>'+
+        '<li class="nav-item"><a class="nav-link" href="#">Kontakt</a></li>'+
+    '</ul>'+'</nav>';
+document.getElementById('navbar').innerHTML = nav;
