9d85983391cbc5588ebdd10ba4969783d62a2ef8
andrzej <andreq114@gmail.com>
2019-03-15 16:01:30 +0100

    working scroll up button

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+let button = document.querySelector('.scroll');
+
+if(button){
+    button.addEventListener('click', goToTop);
+}
+function goToTop() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+function showTopButton() {
+	if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    	document.querySelector(".scroll").style.display = "block";
+    } else {
+    	document.querySelector(".scroll").style.display = "none";
+	}
+}
+window.onscroll = function() {showTopButton()};
