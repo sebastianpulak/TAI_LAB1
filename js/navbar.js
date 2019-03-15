5546fc939fc8a81fad20577df0c3c9a8058c39cd
Artur Górowski <artek159@gmail.com>
2019-03-15 15:24:08 +0100

    restructure

---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+let nav =
+'<nav>'+
+    '<ul class="nav">'+
+        '<li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>'+
+        '<li class="nav-item"><a class="nav-link" href="#">Blog</a></li>'+
+        '<li class="nav-item"><a class="nav-link" href="#">Quiz</a></li>'+
+        '<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>'+
+    '</ul>'+
+'</nav>';
+
+document.getElementById('navbar').innerHTML = nav;
+
+
